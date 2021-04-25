5b32be78215cf23735415a6bdc79422bf72dcd41
Chase Horton <chasehortonj@gmail.com>
2021-04-25 16:47:03 -0500

    Added aboutMe and fizzBuzz

---
+++ fizzBuzz.py
@@ -0,0 +1,19 @@
+#prints fizzBuzz with obfuscation
+
+end = "".join([dir(str)[-1][0] 
+for x in range(2)])
+
+word = "".join([(0.5.__class__.__name__[0]),
+ (9).__class__.__name__[0], end])
+
+word += "".join([True.__class__.__name__[0].upper() + 
+().__class__.__name__[1], end])
+
+print(word)
+
+# Explanation
+# dir returns all methods for class 
+# z is taken from str method zfill
+# __class__.__name__ returns the name of the 
+# class of the object it is attached to
+# classes used are float, int, and tuple
